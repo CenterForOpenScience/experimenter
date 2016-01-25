6921b4b51b2716ee5ae67c71cda4cd44b5d3c51e
Lauren Barker <leb2dg@virginia.edu>
2016-01-25 13:48:37 -0500

    update authenticator

--- app/authenticators/osf-osf-jwt.js
+++ app/authenticators/osf-osf-jwt.js
@@ -4,18 +4,12 @@ import Base from 'ember-simple-auth/authenticators/base';
 
 export default Base.extend({
     authenticate(access_token, expires) {
-        return new Promise(function(resolve, reject) {
-            data: JSON.stringify({data: {
+        return Em.RSVP.resolve(JSON.stringify({data: {
                 type: 'users',
                 attributes: {
                     provider: 'osf',
                     access_token
                 }
-            }})
-        }).then(function(data) {
-            data.data.attributes.accessToken = access_token;
-            return data.data.attributes;
-        });
-
+            }}));
     },
 });
