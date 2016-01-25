c67654954b5adc07226bd1194707303b99281452
Lauren Barker <leb2dg@virginia.edu>
2016-01-25 16:35:46 -0500

    modify index and add adminLTE skin

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	model() {
+    	return ['Experiment1', 'Experiment2', 'Experiment3'];
+  	}
 });
