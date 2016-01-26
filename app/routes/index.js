98a2e87afbb91889d5757f7ed7054a3124f34434
Lauren Barker <leb2dg@virginia.edu>
2016-01-26 11:11:00 -0500

    update static data to mimic real data

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,38 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model() {
-    	return ['Experiment1', 'Experiment2', 'Experiment3'];
-  	}
+    	return [
+    		{
+			    "id": "Experiment1",
+			    "title": "A Test Experiment1",
+			    "description": "This is just for testing.",
+			    "structure": []           
+			}, {
+			    "id": "Experiment2",
+			    "title": "A Test Experiment2",
+			    "description": "This is just for testing.",
+			    "structure": []           
+			}, {
+			    "id": "Experiment3",
+			    "title": "A Test Experiment3",
+			    "description": "This is just for testing.",
+			    "structure": []           
+			}, {
+			    "id": "Experiment4",
+			    "title": "A Test Experiment4",
+			    "description": "This is just for testing.",
+			    "structure": []           
+			}, {
+			    "id": "Experiment5",
+			    "title": "A Test Experiment5",
+			    "description": "This is just for testing.",
+			    "structure": []           
+			}, {
+			    "id": "Experiment6",
+			    "title": "A Test Experiment6",
+			    "description": "This is just for testing.",
+			    "structure": []           
+			}, 
+		];
+	}
 });
