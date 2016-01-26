e61fdf2116e9eb595cb97d30f63b75c31f8d5592
Andy Boughton <abought@gmail.com>
2016-01-26 09:59:01 -0500

    Update models to match sample data

--- app/models/administrator.js
+++ app/models/administrator.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  email: DS.attr('string'),
+  username: DS.attr('string'),
+  password: DS.attr('string'),
 
   experiments: DS.hasMany('experiment'),
   configs: DS.belongsTo('config'),
--- app/models/experiment.js
+++ app/models/experiment.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  title: DS.attr('string'),
+  description: DS.attr('string'),
   structure: DS.attr(),  // TODO: Nested document
-  schema: DS.attr(),  // TODO: Nested document
 
-  administrators: DS.hasMany('administrator'),  // TODO: Is Many to many correct relationship type?
+  administrators: DS.hasMany('administrator'),  // TODO: Is Many-to-many correct relationship type?
   sessions: DS.hasMany('session'),
 });
