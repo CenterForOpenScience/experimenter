5af65e088f216ee3064ae524df02186a77de9c3f
Andy Boughton <abought@gmail.com>
2016-01-29 13:22:27 -0500

    POC of relationships fetching for admin.

--- app/serializers/admin.js
+++ app/serializers/admin.js
@@ -1,7 +1,22 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 import JamSerializer from '../mixins/jam-serializer';
 
 export default DS.JSONAPISerializer.extend(JamSerializer, {
-  modelName: 'admin'
+  modelName: 'admin',
+
+  extractRelationships: function(modelClass, resourceHash) {
+    var relationships = this._super(...arguments);
+    // Convert list of relationship IDs stored as attributes to JSONAPI format
+    var relName = 'experiments';
+    var relData = resourceHash.attributes[relName] || [];
+    relationships[relName] = {
+      data: relData.map(idVal => ({
+          id: idVal,
+          type: Ember.Inflector.inflector.singularize(relName),
+        }))
+    };
+    return relationships;
+  }
 });
