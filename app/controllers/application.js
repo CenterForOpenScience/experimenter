1e31293b1e29dfac92468231b9ca1935cd14d26a
Lauren Barker <leb2dg@virginia.edu>
2016-02-01 12:56:15 -0500

    remove nexted containers, fix indentation, remove menu on login

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	isExpanded: true,
+	isNotLogin: Ember.computed('this.currentPath', function() {
+	  	if (this.currentPath !== "login") {
+	  		return true
+	  	} else {
+	  		return false
+	  	}
+	}),
 
 	actions: {
 	    toggleMenu: function() {
