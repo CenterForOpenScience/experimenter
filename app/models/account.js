d568063ab90c87877b307bd7bfbf629577b254ba
Andy Boughton <abought@gmail.com>
2016-02-02 17:40:57 -0500

    Cleanup and change particiopants to use people

--- app/models/account.js
+++ app/models/account.js
@@ -5,15 +5,14 @@ Manage data about one or more documents in the accounts collection
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  // TODO: Add validators and length limits to fields for UI layer
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  username: DS.attr('string'),
-  phone: DS.attr('phone'),
-  profile: DS.attr(), // Should match config.profileSchema layout
+    // TODO: Add validators and length limits to fields for UI layer
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    username: DS.attr('string'),
+    phone: DS.attr('phone'),
+    profile: DS.attr(), // Should match config.profileSchema layout
+    permissions: DS.attr(),
 
-  history: DS.hasMany('history'),
-  permissions: DS.attr(),
-
-  sessions: DS.hasMany('session')
+    history: DS.hasMany('history'),
+    sessions: DS.hasMany('session')
 });
