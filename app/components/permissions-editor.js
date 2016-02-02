60f7cfbdb5229e3e7cfd6801df391ba4c05b6ff5
Andy Boughton <abought@gmail.com>
2016-02-02 13:01:57 -0500

    Add "remove contributor" functionality to permissions editor

--- app/components/permissions-editor.js
+++ app/components/permissions-editor.js
@@ -1,27 +1,35 @@
 import Ember from 'ember';
 
+// FIXME: Known bug in original- if the server save request fails, the value will appear to have been added until page reloaded.
+//  (need to catch and handle errors)
 let PermissionsEditor = Ember.Component.extend({
-  tagName: 'table',
-  classNames: ['table'],
-  permissionLevels: [
-    'CREATE',
-    'READ',
-    'UPDATE',
-    'DELETE',
-    'ADMIN',
-  ],
+    tagName: 'table',
+    classNames: ['table'],
+    permissionLevels: [
+        'CREATE',
+        'READ',
+        'UPDATE',
+        'DELETE',
+        'ADMIN',
+    ],
 
-  newPermissionLevel: 'CREATE',
-  newPermissionSelector: '',
+    newPermissionLevel: 'CREATE',
+    newPermissionSelector: '',
 
-  actions: {
-    addPermission() {
-      this.permissions[this.get('newPermissionSelector')] = this.get('newPermissionLevel');
-      this.set('newPermissionSelector', '');
-      this.rerender();
-      this.get('onchange')(this.get('permissions'));
+    actions: {
+        addPermission() {
+            this.permissions[this.get('newPermissionSelector')] = this.get('newPermissionLevel');
+            this.set('newPermissionSelector', '');
+            this.rerender();
+            this.get('onchange')(this.get('permissions'));
+        },
+
+        removePermission(selector) {
+            delete this.permissions[selector];
+            this.rerender();
+            this.get('onchange')(this.get('permissions'));
+        },
     }
-  }
 });
 
 PermissionsEditor.reopenClass({
