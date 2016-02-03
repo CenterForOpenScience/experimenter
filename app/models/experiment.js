17ef64ba946f1f1879768b210db197206bd77a8e
Lauren Barker <leb2dg@virginia.edu>
2016-02-03 16:40:00 -0500

    updated experiment model and related documents

--- app/models/experiment.js
+++ app/models/experiment.js
@@ -7,6 +7,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
+  active: DS.attr('string'),
+  beginDate: DS.attr('string'),	// TODO: ISODate
+  endDate: DS.attr('string'),	// TODO: ISODate
+  lastEdited: DS.attr('string'),	// TODO: ISODate
   structure: DS.attr(),  // TODO: Nested document
   permissions: DS.attr(),
 
