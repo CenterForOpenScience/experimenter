59b60522c59995d7fd266dff5c34c854fec3eb9b
Andy Boughton <abought@gmail.com>
2016-02-03 17:14:09 -0500

    Remove history hack (no longer needed)

--- app/mixins/jam-document-serializer.js
+++ app/mixins/jam-document-serializer.js
@@ -7,16 +7,6 @@ export default Ember.Mixin.create({
         return 'documents';
     },
 
-    extractRelationships: function(modelClass, resourceHash) {
-        var relationships = this._super(...arguments);
-        // Manually rebuild the history relationship. This can be removed upon resolution of
-        // issue with the auto-generated history link: ticket https://github.com/CenterForOpenScience/jamdb/issues/3
-        relationships.history = relationships.history || {'links': {}};
-        relationships.history.links.related = 'http://localhost:1212/v1/id/documents/' + resourceHash.id + '/history';
-        relationships.history.links.self = relationships.history.links.related;
-        return relationships;
-    },
-
     extractAttributes: function(modelClass, resourceHash) {
         // Merge meta attributes into the attributes available on model
         var attributes = this._super(...arguments);
