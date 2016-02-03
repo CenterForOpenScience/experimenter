3a7c7de475a604bdc127d1ab866f64f2f70378cd
Andy Boughton <abought@gmail.com>
2016-02-03 17:04:22 -0500

    Re-delete line. Oops.

--- app/router.js
+++ app/router.js
@@ -26,7 +26,6 @@ Router.map(function() {
     this.route('participants');
     this.route('settings');
     this.route('project-settings');
-    this.route('testmodel');
 });
 
 export default Router;
