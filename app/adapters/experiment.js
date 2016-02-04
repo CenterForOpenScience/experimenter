b0169c4c3b5b6e8eeba8168d3fe4992b25342ce3
Lauren Barker <leb2dg@virginia.edu>
2016-02-04 16:02:05 -0500

    edit the queryUrlTemplate for experiments

--- app/adapters/experiment.js
+++ app/adapters/experiment.js
@@ -2,4 +2,5 @@ import ApplicationAdapter from './application';
 import JamCollectionAdapter from '../mixins/jam-collection-adapter';
 
 export default ApplicationAdapter.extend(JamCollectionAdapter, {
+	queryUrlTemplate: '{+host}/{+namespace}/collections{/jamNamespace}.{+collectionId}/documents',
 });
