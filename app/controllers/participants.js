0a43e4662fef56eab8c633fec0a276deb6c04b82
Andy Boughton <abought@gmail.com>
2016-02-04 09:41:23 -0500

    Add skeleton of link for participants/:profile_id page
    (page for nested route not working yet)

--- app/controllers/participants.js
+++ app/controllers/participants.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     // Manually specify column format options for model table
-    columns: [{propertyName: 'id'},
-              {propertyName: 'firstName'},
-              {propertyName: 'lastName'},
-              {propertyName: 'modifiedOn', title: 'Last Active'}],  // TODO: That's not quite what this field does, but useful first approximation
+    columns: [
+        {propertyName: 'id', title:'ID'},
+        {propertyName: 'firstName'},
+        {propertyName: 'lastName'},
+        {propertyName: 'modifiedOn', title: 'Last active'},  // TODO: That's not quite what this field does, but useful first approximation
+        {title: '', template: 'custom/profile-link'}
+    ],
 });
