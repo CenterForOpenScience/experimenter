00ab3284165dc66c2b0837ed993a5732935155f7
Sam Chrisinger <s.chrisinger@gmail.com>
2016-02-04 16:49:51 -0500

    Migrating models/adapters/serializers to exp-addons repo

--- app/adapters/experiment.js
+++
@@ -1,5 +0,0 @@
-import ApplicationAdapter from './application';
-import JamDocumentAdapter from '../mixins/jam-document-adapter';
-
-export default ApplicationAdapter.extend(JamDocumentAdapter, {
-});
