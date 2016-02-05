1cf757559c6800e5fb4fdd0c4434d801231add87
Andy Boughton <abought@gmail.com>
2016-02-05 15:02:47 -0500

    Placeholder for fetching related session data on controller

--- app/routes/participants/profile.js
+++ app/routes/participants/profile.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return this.store.findRecord('profile', params.profile_id);
+    },
+
+    afterModel(resolvedModel, transition) {
+        // Get all sessions associated with this profile
+        this.set('profileSessions', 'placeholder');
+    },
+
+    setupController(controller, model){
+        this._super(...arguments);
+        controller.set('profileSessions', this.get('profileSessions'));
     }
 });
