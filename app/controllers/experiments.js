7b663c087aecaa24922e4b8fb042bc51a02e93e1
Lauren Barker <leb2dg@virginia.edu>
2016-02-08 15:46:35 -0500

    sort experiments by title, beginDate, or endDate in ascending order

--- app/controllers/experiments.js
+++ app/controllers/experiments.js
@@ -3,10 +3,32 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	queryParams: ['active'],
 	active: null,
+	sortingMethod: null,
+
+	sortedExperiments: Ember.computed('sortingMethod', 'model', function() {
+		var sortingMethod = this.get('sortingMethod');
+		var experiments = this.get('model');
+
+		if (sortingMethod) {
+			return experiments.sortBy('experiments', sortingMethod);
+		}
+		return experiments
+	}),
+	// using standard ascending sort
+	// experimentTitleSorting: ['title'],
+	// sortedExperimentsTitle: Ember.computed.sort('experiments', 'experimentTitleSorting'),
+
+	// using descending sort
+	// experimentTitleSortingDesc: ['title:desc'],
+	// sortedExperimentsTitleDesc: Ember.computed.sort('experiments', 'experimentTitleSortingDesc'),
 
 	actions: {
 	    selectStatusFilter: function(status) {
 	      	this.set('active', status);
+	    },
+	    selectSortingMethod: function(sortingMethod) {
+	      	this.set('sortingMethod', sortingMethod);
+	      	this.get('sortedExperiments');
 	    },
 	}
 });
