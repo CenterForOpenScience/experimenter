7099c077910bd3f25e1c2085a26a3a91d417e7d7
Andy Boughton <abought@gmail.com>
2016-02-08 14:23:40 -0500

    Filter to sessions associated with a given profile
    Also show experiment ID for a record without needing a second API call

--- app/routes/participants/profile.js
+++ app/routes/participants/profile.js
@@ -5,7 +5,8 @@ export default Ember.Route.extend({
         return Ember.RSVP.hash(
             {
                 'profile': this.store.findRecord('profile', params.profile_id),
-                'sessions': this.store.findAll('session')
+                'sessions': this.store.query('session',
+                    {filter: {profile: params.profile_id}}),
             });
     }
 });
