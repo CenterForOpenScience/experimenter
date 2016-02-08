93b67882082d64ad95bcd0e88ceb7eb0ec283457
Lauren Barker <leb2dg@virginia.edu>
2016-02-08 09:40:29 -0500

    check for params in route

--- app/routes/experiments.js
+++ app/routes/experiments.js
@@ -1,19 +1,22 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+const {
+	isPresent
+} = Ember;
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	queryParams: {
     	active: {
 	    	refreshModel: true
 	    }
 	},
-    beforeModel() {
-
-    },
     model(params) {
-    	return this.store.query('experiment', {filter: params});
-    	// return this.store.query('experiment', {filter: {active: 'Active'}});
+    	if (isPresent(params.active)) {
+    		return this.store.query('experiment', {filter: params});
+    	}
+    	return this.store.findAll('experiment');
+    	
     	// return this.store.query('experiment', {filter: {title: 'Abomination of Gudul'}});
-    	// return this.store.findAll('experiment');
     }
 });
