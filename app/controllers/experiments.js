5be6d0685806900565c88956325058eb2e270d29
Lauren Barker <leb2dg@virginia.edu>
2016-02-09 16:53:27 -0500

    react to filter input and modify query params accordingly

--- app/controllers/experiments.js
+++ app/controllers/experiments.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	queryParams: ['active'],
+	queryParams: ['active', 'sortProperties', 'match'],
 	active: null,
+	order: 'asc',
+	match: '*',
+
 	sortProperty: 'title',
 	sortOrder: 'asc',
 	sortProperties: Ember.computed('sortProperty', 'sortOrder', function() {
+		if (this.get('sortProperty') == null) {
+			return null
+		}
 		return [`${this.get('sortProperty')}:${this.get('sortOrder')}`];
 	}),
-	sortedExperiments: Ember.computed.sort('model', 'sortProperties'),
 	toggleOrder: function(order) {
 		if (order === 'asc') {
     		this.set('sortOrder', 'desc'); 
@@ -19,6 +24,8 @@ export default Ember.Controller.extend({
 	actions: {
 	    selectStatusFilter: function(status) {
 	      	this.set('active', status);
+	      	this.set('sortProperty', 'title');
+	      	this.set('sortOrder', 'asc');
 	    },
 	    sortingMethod: function(sortProperty) {
 	    	if (this.get('sortProperty') === sortProperty) {
@@ -27,7 +34,16 @@ export default Ember.Controller.extend({
 	    		this.set('sortOrder', 'asc');
 	    	}
 	      	this.set('sortProperty', sortProperty);
-	      	this.get('sortedExperiments');
+	    },
+	    resetParams: function() {
+	    	this.set('active', null);
+	      	this.set('match', '*');
+	      	this.set('sortProperty', 'title');
+	      	this.set('order', 'asc');
+	    },
+	    updateSearch: function(value) {
+	    	this.set('match', value);
+	    	this.set('sortProperty', null);
 	    },
 	}
 });
--- app/routes/experiments.js
+++ app/routes/experiments.js
@@ -9,12 +9,27 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	queryParams: {
     	active: {
 	    	refreshModel: true
-	    }
+	    },
+        sortProperties: {
+            refreshModel: true
+        },
+        q: {
+            refreshModel: true
+        },
+        match: {
+            refreshModel: true
+        },
 	},
     model(params) {
-    	if (isPresent(params.active)) {
-    		return this.store.query('experiment', {filter: params});
-    	}
+        if (isPresent(params.active) && isPresent(params.sortProperties) && isPresent(params.match)) {
+            return this.store.query('experiment', {q: params.match, filter: {active: params.active}, sort: params.sortProperties });
+        }
+        else if (isPresent(params.sortProperties) && isPresent(params.match)) {
+            return this.store.query('experiment', {q: params.match, sort: params.sortProperties});
+        }
+        else if (isPresent(params.match)) {
+            return this.store.query('experiment', {q: params.match});
+        }
     	return this.store.findAll('experiment');
     }
 });
