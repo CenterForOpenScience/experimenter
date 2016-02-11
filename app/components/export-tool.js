5efbeeff5cd523e9494b1a4786c418740eef6f4a
Lauren Barker <leb2dg@virginia.edu>
2016-02-11 16:27:02 -0500

    load data from store into textarea

--- app/components/export-tool.js
+++ app/components/export-tool.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    store: Ember.inject.service(),
+    data: null,
+    processData: function(data) {
+        console.log(data);
+        this.set('data', data.content);
+    },
+    actions: {
+        loadData: function() {
+            var store = this.get('store');
+            var self = this;
+            var data = store.findAll('profile').then( function(results) {
+                console.log(results);
+                self.processData(results);
+            })
+        }
+    }
 });
