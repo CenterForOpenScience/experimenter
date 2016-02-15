6363e299041b74e185fff2ebe60d33b2dd983090
Sam Chrisinger <s.chrisinger@gmail.com>
2016-02-15 12:29:03 -0500

    Show notification when experiment is saved.

--- app/components/experiment-detail.js
+++ app/components/experiment-detail.js
@@ -3,13 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     experiment: null,
     editing: false,
+    toast: Em.inject.service(),
     actions: {
         toggleEditing: function() {
             this.toggleProperty('editing');
             if (!this.get('editing')) {
                 var exp = this.get('experiment');
-                exp.save().then(function() {
-                    debugger;
+                exp.save().then(() => {
+                    this.get('toast.info')('Experiment saved successfully.');
                 });
             }
         }
