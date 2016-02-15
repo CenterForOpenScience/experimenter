734bdceadaa694ff6aba74fb3f52dd55c38adb6a
Lauren Barker <leb2dg@virginia.edu>
2016-02-15 14:09:04 -0500

    implement mapping function on data and define default function

--- app/components/export-tool.js
+++ app/components/export-tool.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
+// import FileSaver from '../bower_components/file-saver.js/FileSaver.js';
 
 export default Ember.Component.extend({
     attributeBindings: ['data', 'mappingFunction'],
+    defaultMappingFunction: function (model) {
+        return model._data;
+    },
     processedData: Ember.computed( 'data', 'mappingFunction', {
         get() {
             var dataArray = this.get('data');
             var mappingFunction = this.get('mappingFunction');
             if (Ember.isPresent(dataArray) && Ember.isPresent(mappingFunction)) {
                 console.log(mappingFunction);
-                return this.convertToJSON(dataArray); 
+                var mapped = dataArray.map(mappingFunction);
+                return this.convertToJSON(mapped); 
             } else if (Ember.isPresent(dataArray)) {
-                return this.convertToJSON(dataArray); 
+                var mapped = dataArray.map(this.get('defaultMappingFunction'));
+                debugger;
+                return this.convertToJSON(mapped); 
             } else {
                 return null;
             }
@@ -24,7 +31,7 @@ export default Ember.Component.extend({
         var jsonData = "";
         
         for (var i = 0; i < dataArray.length; i++) {
-            jsonData = jsonData + JSON.stringify(dataArray[i]._data);
+            jsonData = jsonData + JSON.stringify(dataArray[i]);
         }
         return jsonData;
     },
@@ -45,5 +52,12 @@ export default Ember.Component.extend({
 
         return str;
     },
+    actions: {
+        downloadFile: function () {
+            var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+            // this doesn't work right now
+            FileSaver.saveAs(blob, "hello world.txt");
+        }
+    }
     
 });
