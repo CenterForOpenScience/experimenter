3512e40c1af015fb32af911778c2b73d351e1997
Lauren Barker <leb2dg@virginia.edu>
2016-02-15 11:57:34 -0500

    set optional mapping function as data attribute and pull JSON formatting into function

--- app/components/export-tool.js
+++ app/components/export-tool.js
@@ -2,28 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     attributeBindings: ['data', 'mappingFunction'],
-    processedData: Ember.computed( 'data', {
+    processedData: Ember.computed( 'data', 'mappingFunction', {
         get() {
             var dataArray = this.get('data');
-            if (Ember.isPresent(dataArray)) {
-                console.log(dataArray);
-                // var dataArray = data.content;
-                var jsonData = "";
-                
-                for (var i = 0; i < dataArray.length; i++) {
-                    jsonData = jsonData + JSON.stringify(dataArray[i]._data);
-                }
-                //var csvData = this.convertToCSV(dataArray);
-                return jsonData; 
+            var mappingFunction = this.get('mappingFunction');
+            if (Ember.isPresent(dataArray) && Ember.isPresent(mappingFunction)) {
+                console.log(mappingFunction);
+                return this.convertToJSON(dataArray); 
+            } else if (Ember.isPresent(dataArray)) {
+                return this.convertToJSON(dataArray); 
             } else {
                 return null;
             }
-            
         }, 
         set(_, value) {
             this.set('processedData', value);
         }
     }),
+    convertToJSON: function (dataArray) {
+        console.log(dataArray);
+        var jsonData = "";
+        
+        for (var i = 0; i < dataArray.length; i++) {
+            jsonData = jsonData + JSON.stringify(dataArray[i]._data);
+        }
+        return jsonData;
+    },
     convertToCSV: function (objArray) {
         var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
         var str = '';
