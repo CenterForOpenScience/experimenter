983f1ba9f12774324e517ca4be1afcb62c85007b
Andy Boughton <abought@gmail.com>
2016-02-16 13:37:32 -0500

    Convert lingering references--> profileId

--- app/controllers/participants/index.js
+++ app/controllers/participants/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     // Manually specify column format options for model table
     columns: [
-        {propertyName: 'id', title:'ID'},
+        {propertyName: 'profileId', title:'ID'},
         {propertyName: 'firstName'},
         {propertyName: 'lastName'},
         {propertyName: 'modifiedOn', title: 'Last active'},  // TODO: That's not quite what this field does, but useful first approximation
--- app/routes/participants/profile.js
+++ app/routes/participants/profile.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
     model(params) {
         return Ember.RSVP.hash(
             {
-                'profile': this.store.query('account', {'profiles.profileId': params.profile_id}),   // TODO: Requires globally unique profile IDs- format <acctShortId>.profileId
+                'account': this.store.query('account', {filter: {'profiles.profileId': params.profile_id}}),   // TODO: Requires globally unique profile IDs- format <acctShortId>.profileId
                 'sessions': this.store.query('session',
                     {filter: {profileId: params.profile_id}}),
             });
