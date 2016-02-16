0521fb2a977d30ec6067227e8e9d990af4cdf5a0
Andy Boughton <abought@gmail.com>
2016-02-16 15:43:21 -0500

    Get profile page to display both profile and account data

--- app/routes/participants/profile.js
+++ app/routes/participants/profile.js
@@ -2,14 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
+        this.set('params', params);
         return Ember.RSVP.hash(
             {
                 'account': this.store.query('account', {filter: {'profiles.profileId': params.profile_id}}).then(function(items) {
-                    var parentAccount = items.toArray()[0];
-                    return parentAccount.profileById(params.profile_id);
-                }),   // TODO: Requires globally unique profile IDs- format <acctShortId>.profileId
+                    // Turn query into a single result
+                    return items.toArray()[0];
+                }),   // TODO: Finding profile requires globally unique profile IDs- format <acctShortId>.profileId
                 'sessions': this.store.query('session',
                     {filter: {profileId: params.profile_id}}),
             });
-    }
+    },
+
+    setupController(controller, model) {
+        // Save params so we can fetch specific profile data from account
+        controller.set('params', this.get('params'));
+        this._super(...arguments);
+    },
 });
