c9d85ad4c101739d9a2d8ffa6468b657363ce8e3
Andy Boughton <abought@gmail.com>
2016-02-17 17:43:02 -0500

    Add sample page where sessions data gets fetched from appropriate collection

--- app/routes/experiment.js
+++ app/routes/experiment.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return this.store.find('experiment', params.experiment_id);
+    },
+
+    setupController(controller, model) {
+        // When experiment loaded, ensure there are corresponding session models
+        model._registerSessionModels();
+        var collId = model.get('sessionCollectionId');
+        var thisQuery = this.store.findAll(collId).then(function() {
+            controller.set('someSessions');
+        });
+
+        this._super(...arguments)
     }
 });
