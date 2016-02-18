5ce2900ecde1115413d2b0713e5601ee23d17617
Andy Boughton <abought@gmail.com>
2016-02-18 14:37:52 -0500

    Simplest possible transition abort (plus notes)

--- app/routes/testplayer.js
+++ app/routes/testplayer.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return this.store.findRecord('experiment', 'experimenter.experiments.test0');
+    },
+
+    actions: {
+        willTransition: function(transition) {
+            // FIXME: This won't prevent back button or manual URL change. See https://guides.emberjs.com/v2.3.0/routing/preventing-and-retrying-transitions/#toc_preventing-transitions-via-code-willtransition-code
+            // TODO: Make this part of player?
+            // TODO: style modal
+            // TODO: Be able to check for dirty state first
+            if (!confirm('Are you sure you want to exit the experiment?')) {
+                transition.abort();
+            } else {
+                // Bubble this action to parent routes
+                return true;
+            }
+        }
     }
 });
