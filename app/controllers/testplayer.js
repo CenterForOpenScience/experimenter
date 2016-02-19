94c8fd3dbaf11e1de870cb36c18013983fe6a88a
Andy Boughton <abought@gmail.com>
2016-02-19 14:49:13 -0500

    willTransition hook should check model dirty state before saving
    Sample test:
    1. Load the page, and immediately click on a link elsewhere (eg "experiments" tab on side menu)
    2. Go through the two frames. Click next on the second frame, which will trigger an invisible save for now.
    3. Try to click the experiments tab again; confirm the transition is allowed because the model is not dirty.

--- app/controllers/testplayer.js
+++ app/controllers/testplayer.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    isDirty() {
+    isDirty: function() {
         // TODO: check the session model to see if it contains unsaved data
+        var model = this.get('model.blankSession');
+        return model.get('hasDirtyAttributes');
     },
     actions: {
         saveSession(payload) {
@@ -10,7 +12,8 @@ export default Ember.Controller.extend({
             console.log('controller is attempting to save');
             var model = this.get('model.blankSession');
             model.setProperties(payload);
-            model.save();  // TODO: Do we need to set model with changed result?
+            model.save();
+            // this.set('model', model);  //  TODO: Update model on controller
         }
     }
 });
--- app/routes/testplayer.js
+++ app/routes/testplayer.js
@@ -17,10 +17,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     actions: {
         willTransition: function(transition) {
             // FIXME: This won't prevent back button or manual URL change. See https://guides.emberjs.com/v2.3.0/routing/preventing-and-retrying-transitions/#toc_preventing-transitions-via-code-willtransition-code
-            // TODO: Make this part of player?
-            // TODO: style modal
-            // TODO: Be able to check for dirty state first
-            if (!confirm('Are you sure you want to exit the experiment?')) { // TODO: Check dirty state?
+            if (this.controller.isDirty() && !confirm('Are you sure you want to exit the experiment?')) {
                 transition.abort();
             } else {
                 // Bubble this action to parent routes
