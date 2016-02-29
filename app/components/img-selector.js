a0f8d139e63d4f85d47ae1c644d69217a8e0b075
Sam Chrisinger <s.chrisinger@gmail.com>
2016-02-29 16:21:01 -0500

    Remove old commented-out code

--- app/components/img-selector.js
+++ app/components/img-selector.js
@@ -6,20 +6,8 @@ export default Ember.Component.extend({
         uploadImage: function(e) {
             var self = this;
 
-            //var canvas = this.$().find('.img-selector-canvas')[0];
-            //var ctx = canvas.getContext('2d');
-
             var reader = new window.FileReader();
             reader.onload = function(event){
-                /*
-                var img = new Image();
-                img.onload = function(){
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    ctx.drawImage(img,0,0);
-                };
-                img.src = event.target.result;
-                 */
                 self.set('thumbnail', event.target.result);
                 var onSetImage = self.get('onSetImage');
                 if (onSetImage) {
