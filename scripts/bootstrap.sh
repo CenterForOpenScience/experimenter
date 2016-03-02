f9c044bbf2d5c821c4d99d52fbc9936b47b21d85
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-02 11:30:00 -0500

    Improvements to bootstrap.sh
    - remove auto-generated schemas (to prevent loading old schemas)
    - run `jam delete experimenter` automatically
    - run `jam userify experimenter accounts` automatically

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,8 +6,15 @@ source ~/.bashrc
 
 workon `lsvirtualenv -b |grep jam`
 
+rm ./schemas/*
+
+jam delete experimenter
 jam create experimenter
+
 jam create experimenter sys
 jam update experimenter -p "jam-experimenter:sys-root ADMIN"
 jam userify experimenter sys
 echo '{"password":"$2b$12$iujjM4DtPMWVL1B2roWjBeHzjzxaNEP8HbXxdZwRha/j5Pc8E1n2G"}' | jam create experimenter sys root
+
+jam create experimenter accounts
+jam userify experimenter accounts
