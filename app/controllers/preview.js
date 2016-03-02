85b9b932a0e15b254203614cae55077a3945deef
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-02 10:32:09 -0500

    Add session to preview controller and pass session down to exp-player

--- app/controllers/preview.js
+++ app/controllers/preview.js
@@ -1,19 +1,40 @@
-
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    model: null,
+    store: Ember.inject.service(),
     isDirty: function() {
         // TODO: check the session model to see if it contains unsaved data
-        var model = this.get('model.blankSession');
-        return model.get('hasDirtyAttributes');
+        var session = this.get('session');
+        return session.get('hasDirtyAttributes');
     },
     actions: {
         saveSession(payload) {
             // Save a provided javascript object to a session object
-            var model = this.get('model.blankSession');
-            model.setProperties(payload);
-            model.save();
-            this.set('model.blankSession', model);
+            var session = this.get('session');
+            session.setProperties(payload);
+            session.save();
+            this.set('session', session);
         }
-    }
+    },
+    _session: null,
+    session: Ember.computed('model', {
+        get: function() {
+            var session = this.get('store').createRecord(this.get('model.sessionCollectionId'), {
+                experimentId: this.get('experiment.id'),
+                profileId: 'tester0.prof1', // TODO fetch from service
+                profileVersion: '',
+                softwareVersion: ''
+            });
+            this.get('model').getCurrentVersion().then(function(versionId) {
+                session.set('experimentVersion', versionId);
+            });
+            this.set('session', session);
+            return session;
+        },
+        set: function(session) {
+            this.set('_session', session);
+            return session;
+        }
+    })
 });
