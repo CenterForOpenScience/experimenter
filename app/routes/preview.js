0614b7929501192cfa5f61c380f6d233c518724a
Andy Boughton <abought@gmail.com>
2016-03-03 11:10:56 -0500

    Consolidate testplayer code into preview route

--- app/routes/preview.js
+++ app/routes/preview.js
@@ -1,17 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function(params) {
-        return this.store.findRecord('experiment', params.experiment_id);
+    model(params) {
+        return this.store.findRecord('experiment', params.experiment_id).then((experiment) => {
+            // When page loads, creates new session, but doesn't save to store
+            var session = this.store.createRecord(experiment.get('sessionCollectionId'), {
+                experimentId: experiment.id,
+                profileId: 'tester0.prof1', // TODO fetch from service
+                profileVersion: '',
+                softwareVersion: ''
+            });
+            experiment.getCurrentVersion().then(function(versionId) {
+                session.set('experimentVersion', versionId);
+            });  // TODO: May be an edge case where experimentVersion isn't set/ resolved before this hash returns
+            return Ember.RSVP.hash({
+                experiment: experiment,
+                session: session
+            });
+        });
     },
-    willTransition: function(transition) {
-        // FIXME: This won't prevent back button or manual URL change. See https://guides.emberjs.com/v2.3.0/routing/preventing-and-retrying-transitions/#toc_preventing-transitions-via-code-willtransition-code
-        if (this.controller.isDirty() && !confirm('Are you sure you want to exit the experiment?')) {
-            transition.abort();
-            return false;
-        } else {
-            // Bubble this action to parent routes
-            return true;
+
+    actions: {
+        willTransition: function(transition) {
+            // FIXME: This won't prevent back button or manual URL change. See https://guides.emberjs.com/v2.3.0/routing/preventing-and-retrying-transitions/#toc_preventing-transitions-via-code-willtransition-code
+            if (this.controller.isDirty() && !confirm('Are you sure you want to exit the experiment?')) {
+                transition.abort();
+                return false;
+            } else {
+                // Bubble this action to parent routes
+                return true;
+            }
         }
     }
 });
