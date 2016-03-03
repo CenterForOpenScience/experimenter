b6961523008d8ae7c398a1b54ab4d4d993d247b6
Andy Boughton <abought@gmail.com>
2016-03-03 11:22:33 -0500

    Remove deprecated testplayer route

--- app/router.js
+++ app/router.js
@@ -31,7 +31,6 @@ Router.map(function() {
     this.route('settings');
     this.route('project-settings');
 
-    this.route('testplayer');
     this.route('testmodel');
 
     this.route('preview', {path: '/preview/:experiment_id/'});
