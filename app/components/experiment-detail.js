39fcc5074c6ad0d76c905aef91a122e2bad64b0c
Andy Boughton <abought@gmail.com>
2016-03-03 22:10:48 -0500

    Use ember methods where possible (clearer intent)

--- app/components/experiment-detail.js
+++ app/components/experiment-detail.js
@@ -26,14 +26,14 @@ export default Ember.Component.extend({
             var exp = this.get('experiment');
             exp.set('state', exp.ACTIVE);
             exp.save().then(() => {
-                this.get('toast.info')('Experiment started successfully.');
+                this.toast.info('Experiment started successfully.');
             });
         },
         delete: function() {
             var exp = this.get('experiment');
             exp.set('state', exp.DELETED);
             exp.save().then(() => {
-                this.get('onDelete')();
+                this.sendAction('onDelete');
             });
         },
         clone: function() {
@@ -42,14 +42,14 @@ export default Ember.Component.extend({
             expData.title = `Copy of ${expData.title}`;
             var clone = this.get('store').createRecord('experiment', expData);
             clone.save().then(() => {
-                this.get('onClone')(clone);
+                this.sendAction('onClone', clone);
             });
         },
         onSetImage: function(thumbnail) {
             var exp = this.get('experiment');
             exp.set('thumbnail', thumbnail);
             exp.save().then(() => {
-                this.get('toast.info')('Thumbnail updated successfully.');
+                this.toast.info('Thumbnail updated successfully.');
             });
         }
     }
