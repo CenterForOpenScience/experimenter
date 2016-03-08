beaaab7f4b29a0c63675fcc18acbb6a8222588f7
Chris Seto <chriskseto@gmail.com>
2016-03-08 14:16:01 -0500

    Small updates

--- app/initializers/register-ace-editor.js
+++
@@ -1,9 +0,0 @@
-export function initialize(app) {
-    app.register('ace:main', window.ace, {instantiate: false});
-    app.inject('component:ace-editor', 'ace', 'ace:main');
-}
-
-export default {
-    name: 'register-ace-editor',
-    initialize
-};
