d82d9c91758150f36786b1e27fd30608c468bf3f
Andy Boughton <abought@gmail.com>
2016-03-08 15:03:58 -0500

    Adding and removing users work, but page doesn't rerender

--- app/components/permissions-editor.js
+++ app/components/permissions-editor.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import {adminPattern} from  '../utils/patterns';
 
 // FIXME: Known bug in original- if the server save request fails, the value will appear to have been added until page reloaded.
 //  (need to catch and handle errors)
@@ -9,21 +10,35 @@ let PermissionsEditor = Ember.Component.extend({
     newPermissionLevel: 'ADMIN',
     newPermissionSelector: '',
 
+    usersList: Ember.computed('permissions', function() {
+        var permissions = this.get('permissions');
+
+        // Assumption: all properties passed into this page will match admin pattern
+        return Object.getOwnPropertyNames(permissions).map(function(key){
+            return adminPattern.exec(key)[1];
+        });
+    }),
+
     actions: {
         addPermission() {
             var userId = this.get('newUserId');
 
             this.permissions[`user-osf-${userId}`] = this.get('newPermissionLevel');
             this.set('newUserId', '');
-            this.rerender();
             this.sendAction('onchange', this.get('permissions'));
+            this.set('permissions', this.permissions);
+            this.rerender();
+
         },
 
         removePermission(userId) {
             var selector = `user-osf-${userId}`;
-            delete this.permissions[selector];
+            var permissions = this.get('permissions');
+
+            delete permissions[selector];
+            this.sendAction('onchange', permissions);
+            this.set('permissions', permissions);
             this.rerender();
-            this.sendAction('onchange', this.get('permissions'));
         }
     }
 });
--- app/controllers/project-settings.js
+++ app/controllers/project-settings.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
+import {adminPattern} from  '../utils/patterns';
 
-// Admin users are those authenticated via OSF
-var adminPattern = new RegExp(/^user-osf-(\w+|\*)$/);
 
 export default Ember.Controller.extend({
     filteredPermissions: Ember.computed('model', function() {
@@ -16,10 +15,11 @@ export default Ember.Controller.extend({
     }),
 
     /* Return permissions strings that correspond to admin users (those who log in via OSF) */
-    userPermissions:  Ember.computed('filteredPermissions', function() {
+    _userPermissions:  Ember.computed('filteredPermissions', function() {
         var filtered = this.get('filteredPermissions');
         return filtered[0];
     }),
+    userPermissions: Ember.computed.readOnly('_userPermissions'), // TODO: is this necessary?
 
     /* Return permissions strings that do not match OSF users */
     systemPermissions:  Ember.computed('filteredPermissions', function() {
@@ -32,7 +32,6 @@ export default Ember.Controller.extend({
             // Save updated permissions, and avoid overwriting system-level permissions not displayed to the user
 
             var payload = Object.assign({}, permissions, this.get('systemPermissions'));
-            console.log('permissions', this.get('userPermissions'), this.get('systemPermissions'), payload);
             this.set('model.permissions', payload);
             this.get('model').save();
         }
