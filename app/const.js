37fd38e31c99d594f23e271483d85275d3ba3fd9
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-09 14:35:20 -0500

    " -> '

--- app/const.js
+++ app/const.js
@@ -1,58 +1,58 @@
 export const SESSIONSCHEMA = {
-    "type": "jsonschema",
-    "schema": {
-        "type": "object",
-        "properties": {
-            "profileId": {
-                "id": "profileId",
-                "type": "string",
-                "pattern": "\\w+\\.\\w+"
+    'type': 'jsonschema',
+    'schema': {
+        'type': 'object',
+        'properties': {
+            'profileId': {
+                'id': 'profileId',
+                'type': 'string',
+                'pattern': '\\w+\\.\\w+'
             },
-            "profileVersion": {
-                "id": "profileVersion",
-                "type": "string"
+            'profileVersion': {
+                'id': 'profileVersion',
+                'type': 'string'
             },
-            "experimentId": {
-                "id": "experimentId",
-                "type": "string",
-                "pattern": "\\w+\\.\\w+\\.\\w+"
+            'experimentId': {
+                'id': 'experimentId',
+                'type': 'string',
+                'pattern': '\\w+\\.\\w+\\.\\w+'
             },
-            "experimentVersion": {
-                "id": "experimentVersion",
-                "type": "string"
+            'experimentVersion': {
+                'id': 'experimentVersion',
+                'type': 'string'
             },
-            "sequence": {
-                "id": "sequence",
-                "type": "array",
-                "items": {
-                    "type": "string"
+            'sequence': {
+                'id': 'sequence',
+                'type': 'array',
+                'items': {
+                    'type': 'string'
                 }
             },
-            "softwareVersion": {
-                "id": "softwareVersion",
-                "type": "string"
+            'softwareVersion': {
+                'id': 'softwareVersion',
+                'type': 'string'
             },
-            "expData": {
-                "id": "expData",
-                "type": "object"
+            'expData': {
+                'id': 'expData',
+                'type': 'object'
             },
-            "feedback": {
-                "id": "feedback",
-                "type": "string"
+            'feedback': {
+                'id': 'feedback',
+                'type': 'string'
             },
-            "hasReadFeedback": {
-                "id": "hasReadFeedback",
-                "type": "boolean"
+            'hasReadFeedback': {
+                'id': 'hasReadFeedback',
+                'type': 'boolean'
             }
         },
-        "required": [
-            "profileId",
-            "profileVersion",
-            "experimentId",
-            "experimentVersion",
-            "sequence",
-            "softwareVersion",
-            "expData"
+        'required': [
+            'profileId',
+            'profileVersion',
+            'experimentId',
+            'experimentVersion',
+            'sequence',
+            'softwareVersion',
+            'expData'
         ]
     }
 };
