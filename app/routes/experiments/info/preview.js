8459fdf88cdd3b380296d73f641495199590ebc1
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-10 07:38:57 -0500

    Use ExpPalayerRoute mixin in preview route

--- app/routes/experiments/info/preview.js
+++ app/routes/experiments/info/preview.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-import PastSessionsRouteMixin from 'exp-player/mixins/past-sessions-route';
+import ExpPlayerRouteMixin from 'exp-player/mixins/exp-player-route';
 
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, PastSessionsRouteMixin, {
-    model(params) {
-        let experiment = this.modelFor('experiments.info');
-        let session = this.store.createRecord(experiment.get('sessionCollectionId'), {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ExpPlayerRouteMixin, {
+    _getExperiment() {
+        return new Ember.RSVP.Promise((resolve) => {
+            resolve(this.modelFor('experiments.info'));
+        });
+    },
+    _getSession(params, experiment) {
+        var session = this.store.createRecord(experiment.get('sessionCollectionId'), {
             experimentId: experiment.id,
             profileId: 'tester0.prof1', // TODO fetch from service
             profileVersion: '',
@@ -15,19 +18,10 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, PastSessionsRouteMixi
             expData: {},
             sequence: []
         });
-
-        // TODO: May be an edge case where experimentVersion isn't set/ resolved before this hash returns
-        return experiment.getCurrentVersion().then(versionId => {
-            session.set('experimentVersion', versionId);
-            return session.save().then(() => session);
+        return new Ember.RSVP.Promise((resolve) => {
+            resolve(session);
         });
     },
-
-    setupController: function(controller, model) {
-        controller.set('experiment', this.modelFor('experiments.info'));
-        return this._super(...arguments);
-    },
-
     actions: {
         willTransition: function(transition) {
             // FIXME: This won't prevent back button or manual URL change. See https://guides.emberjs.com/v2.3.0/routing/preventing-and-retrying-transitions/#toc_preventing-transitions-via-code-willtransition-code
