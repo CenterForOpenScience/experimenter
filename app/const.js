857d529b011f739632b40b53690e8d63683899b2
Andy Boughton <abought@gmail.com>
2016-03-14 17:15:54 -0400

    Consistent typeof mechanism for sessions

--- app/const.js
+++ app/const.js
@@ -7,48 +7,45 @@ export const SESSIONSCHEMA = {
                 'type': 'boolean'
             },
             'profileId': {
-                'id': 'profileId',
                 'type': 'string',
                 'pattern': '\\w+\\.\\w+'
             },
             'experimentId': {
-                'id': 'experimentId',
                 'type': 'string',
                 'pattern': '\\w+\\.\\w+\\.\\w+'
             },
             'experimentVersion': {
-                'id': 'experimentVersion',
                 'type': 'string'
             },
             'sequence': {
-                'id': 'sequence',
                 'type': 'array',
                 'items': {
                     'type': 'string'
                 }
             },
             'expData': {
-                'id': 'expData',
                 'type': 'object'
             },
             'feedback': {
                 '$oneOf': [{
-                    'id': 'feedback',
                     'type': 'string'
                 }, null]
             },
             'hasReadFeedback': {
                 '$oneOf': [{
-                    'id': 'hasReadFeedback',
                     'type': 'boolean'
                 }, null]
             },
             'earlyExit': {
-                'id': 'earlyExit',
-                'type': ['object', 'null'],
+                '$oneOf': [{
+                    'type': 'object'
+                }, null],
                 'properties': {
                     'reason': {
-                        'type': ['string', 'null']
+                        '$oneOf': [{
+                            'type': 'string'
+                        }, null]
+
                     },
                     'privacy': {
                         'type': 'string'
