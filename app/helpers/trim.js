9ac000ccd2a80c880f58dc18be21164a7111c37e
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-21 15:44:36 -0400

    Only let experiment be created if title is non-empty

---
+++ app/helpers/trim.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function trim(params /*, hash*/ ) {
+    return params[0].toString().trim();
+}
+
+export default Ember.Helper.helper(trim);
---
+++ tests/unit/helpers/trim-test.js
@@ -0,0 +1,10 @@
+import { trim } from 'experimenter/helpers/trim';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | trim');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = trim([42]);
+  assert.ok(result);
+});
