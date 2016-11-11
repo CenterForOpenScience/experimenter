671cb7158e453305655faaa274fae54ad54b08f8
Andy Boughton <abought@gmail.com>
2016-11-11 17:41:22 -0500

    Fetch all available results

--- app/routes/experiments/info/results/all.js
+++ app/routes/experiments/info/results/all.js
@@ -1,14 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        //TODO: Get every record EVER, and maybe implement a loading substate template?
-        const experiment = this.modelFor('experiments.info');
-        return this.store.query(experiment.get('sessionCollectionId'),
-            {
+    /**
+     *
+     * @param {String} collectionName Name of the collection to query
+     * @param {Array} dest An array to be used for storing the combined results of all requests
+     * @param {Number} page The current page number to fetch
+     * @returns {Promise} A (chained) promise that will resolve to dest when all records have been fetched
+     * @private
+     */
+    _fetchResults(collectionName, dest, page) {
+        const options = {
                 'filter[completed]': 1,
-                'page[size]': 100
-            });
+                'page[size]': 3,
+                'page': page
+        };
+        return this.store.query(collectionName, options).then(res => {
+            const theseResults = res.toArray();
+            dest.push(...theseResults);
+            // TODO: This is an imperfect means of identifying the last page, but JamDB doesn't tell us directly
+            if (theseResults.length !== 0 && dest.length <  res.get('meta.total')) {
+                return this._fetchResults(collectionName, dest, page + 1);
+            } else {
+                return dest;
+            }
+        });
+    },
+
+    model() {
+        const collectionName = this.modelFor('experiments.info').get('sessionCollectionId');
+        const results = Ember.A();
+        return this._fetchResults(collectionName, results, 1);
     },
 
     setupController(controller) {
