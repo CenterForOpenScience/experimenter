4d1f4dbb704b7da204670b953322ea82157ff595
Andy Boughton <abought@gmail.com>
2016-11-28 13:38:58 -0500

    Don't use private method
    [ci skip]
    [#LEI-313]

--- app/routes/project-settings.js
+++ app/routes/project-settings.js
@@ -4,6 +4,6 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     namespaceConfig: Ember.inject.service(),
     model() {
-        return this.store.find('namespace', this.get('namespaceConfig').get('namespace'));
+        return this.store.findRecord('namespace', this.get('namespaceConfig').get('namespace'));
     }
 });
