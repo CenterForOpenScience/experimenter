41b7efc648ba24d3a11317f78b15fc4d7cd74f7d
Andy Boughton <abought@gmail.com>
2016-12-02 11:41:33 -0500

    Fix issue  with wrong permissions strings
    Apparently, `namespace` is (at least for controllers) a reserved word, and the global namespace property was overriding the value being set.
    
    [#LEI-313]

--- app/utils/patterns.js
+++ app/utils/patterns.js
@@ -3,14 +3,13 @@
  */
 
 // Admin users are those authenticated via OSF
-// TODO: This wass a regex, now it's just a prefix
 const adminPattern = 'user-osf';
 
 /**
  * Create a regex object for matching users based on the specified external auth or Jam collection prefix.
  *
  * @param {String} prefix Eg 'user-osf'
- * @return {RegExp} A new regex object
+ * @return {RegExp} A new regex object that matches `prefix-abc`, `prefix.*`, or similar.
  */
 function makeUserPattern(prefix) {
     return new RegExp(`^${prefix}-(\\w\+|\\*)$`);
