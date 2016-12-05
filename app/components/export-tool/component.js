677cb426b64d0e7be3b0d2198c4173d99a7c1037
Barrett K. Harber <barrett.harber@gmail.com>
2016-12-05 14:18:35 -0500

    Update export tool and tests

--- app/components/export-tool/component.js
+++ app/components/export-tool/component.js
@@ -3,33 +3,63 @@ import Ember from 'ember';
 /**
  * @module experimenter
  * @submodule components
-  */
+ */
 
-// Make sure that ember-data objects are serialized to a JS object
+/**
+ * Make sure that ember-data objects are serialized to a JS object
+ *
+ * @param obj
+ * @returns {*}
+ */
 function serializeItem(obj) {
     if (obj.serialize) {
-        var serialized = obj.serialize();
-        obj = serialized.data.attributes;
+        obj = obj.serialize().data.attributes;
     }
     return obj;
 }
 
-// Flatten a nested object into a single level, with dotted paths for keys
+/**
+ * Flatten a nested object into a single level, with dotted paths for keys
+ *
+ * @param obj
+ * @param prefix
+ * @returns {Object}
+ */
 function squash(obj, prefix) {
-    var ret = {};
-    Object.keys(obj).forEach((key) => {
-        var value = Ember.get(obj, key);
+    let ret = {};
+
+    for (const key in obj) {
+        if (!obj.hasOwnProperty(key)) {
+            continue;
+        }
+
+        const newPrefix = prefix ? `${prefix}.${key}` : key;
+
+        let value = Ember.get(obj, key);
+
         if (value && value.toJSON) {
             value = value.toJSON();
         }
 
         if (Ember.$.isPlainObject(value)) {
-            ret = Ember.$.extend({}, ret, squash(value, prefix ? `${prefix}.${key}` : key));
+            Object.assign(ret, squash(value, newPrefix));
         } else {
-            ret[prefix ? `${prefix}.${key}` : key] = value;
+            ret[newPrefix] = value;
         }
-    });
+    }
+
     return ret;
+}
+
+/**
+ * Makes a value safe for a CSV
+ *
+ * @param val {boolean|null|number|undefined|string} - The value to serialize
+ * @returns {string}
+ */
+function csvSafe(val) {
+    const value = JSON.stringify(val);
+    return value ? value.replace(/\\"/g, '""') : '';
 }
 
 /**
@@ -38,72 +68,99 @@ function squash(obj, prefix) {
  */
 export default Ember.Component.extend({
     /**
-     * @property data The data to be serialized
+     * @property {null|Array<Object>} data The data to be serialized
      */
     data: null,
 
     /**
      * Mapping function to transform a given (squashed) record. Should accept a single argument,
      *  a (possibly nested) JS object of fields
-     * @property {function} mappingFunction
-     * @default Return the item unchanged
+     *
+     * @property {function|null} mappingFunction
+     * @default null
      */
-    mappingFunction(item) {
-        return item;
-    },
+    mappingFunction: null,
 
+    /** @property {('JSON'|'CSV')} the data format */
     dataFormat: 'JSON',
-    // Recognized data formats. Hash of form {displayValue: Extension} items
+
+    /**
+     * Recognized data formats. Hash of form `{displayValue: Extension}` items
+     *
+     * @enum {string}
+     */
     dataFormats: {
         JSON: 'JSON',
         CSV: 'CSV',
         'CSV (for ISP)': 'CSV'
     },
 
-    processedData: Ember.computed('data', 'dataFormat', function() {
-        var data = this.get('data') || [];
+    /**
+     * @property {null|string} The processed data
+     */
+    processedData: Ember.computed('data', 'dataFormat', 'mappingFunction', function() {
+        let data = this.get('data') || [];
         if (data.toArray) {
             data = data.toArray();
         }
         let dataArray = data.map(serializeItem);
 
-        var dataFormat = this.get('dataFormat');
-        var mappingFunction = this.get('mappingFunction');
+        const dataFormat = this.get('dataFormat');
+        const mappingFunction = this.get('mappingFunction');
 
         if (Ember.isPresent(dataArray)) {
-            let mapped = dataArray.map(mappingFunction);
+            const mapped = mappingFunction ? dataArray.map(mappingFunction) : dataArray;
             return this.convertToFormat(mapped, dataFormat);
-        } else {
-            return null;
         }
+
+        return null;
     }),
 
+    /**
+     * Converts an array to a JSON string
+     *
+     * @param dataArray {!Array<Object>} - The rows of the CSV
+     * @private
+     * @returns {string}
+     */
     _convertToJSON(dataArray) {
         return JSON.stringify(dataArray, null, 4);
     },
+
+    /**
+     * Converts an array to a standard CSV file
+     *
+     * @param dataArray {!Array<Object>} - The rows of the CSV
+     * @private
+     * @returns {string}
+     */
     _convertToCSV(dataArray) {
         // Flatten the dictionary keys for readable column headers
-        let squashed = dataArray.map((item => squash(item)));
+        const squashed = dataArray.map(item => squash(item));
 
-        var fields = Object.keys(squashed[0]);
-        var csv = [fields.join(',')];
+        const fields = Object.keys(squashed[0]);
+        const csv = [fields.join()];
 
-        squashed.forEach((item) => {
-            var line = [];
-            fields.forEach(function(field) {
-                var value = JSON.stringify(item[field]);
-                if (value) {
-                    value = value.replace(/\\"/g, '""');
-                } else {
-                    value = '';
-                }
-                line.push(value);
-            });
-            csv.push(line.join(','));
-        });
-        csv = csv.join('\r\n');
-        return csv;
+        for (const item of squashed) {
+            const line = [];
+
+            for (const field of fields) {
+                line.push(csvSafe(item[field]));
+            }
+
+            csv.push(line.join());
+        }
+
+        return csv.join('\r\n');
     },
+
+    /**
+     * Converts an array to a standard CSV file
+     *
+     * @param dataArray {!Array<Object>} - The rows of the CSV
+     * @private
+     * @returns {string}
+     */
     _convertToISP(dataArray) {
         // ISP-specific CSV file format
 
@@ -112,8 +169,10 @@ export default Ember.Component.extend({
         // Then serialize to CSV
         dataArray = dataArray.map((record) => {
             let newRecord = {};
+
             for (let frameId of Object.keys(record.expData)) {
                 let responses = record.expData[frameId].responses || {};
+
                 for (let question of Object.keys(responses)) {
                     if (frameId === '3-3-rating-form') {
                         for (let item of Object.keys(responses[question])) {
@@ -124,30 +183,52 @@ export default Ember.Component.extend({
                     }
                 }
             }
+
             return newRecord;
         });
         return this._convertToCSV(dataArray);
     },
+
+    /**
+     * Converts an array to the specified format
+     *
+     * @param dataArray {!Array<Object>} - The rows
+     * @param format {!dataFormats} - The conversion output format
+     * @returns {string}
+     */
     convertToFormat(dataArray, format) {
-        if (format === 'JSON') {
-            return this._convertToJSON(dataArray);
-        } else if (format === 'CSV') {
-            return this._convertToCSV(dataArray);
-        } else if (format === 'CSV (for ISP)') {
-            return this._convertToISP(dataArray);
-        } else {
-            throw 'Unrecognized file format specified';
+        switch (format) {
+            case 'JSON':
+                return this._convertToJSON(dataArray);
+            case 'CSV':
+                return this._convertToCSV(dataArray);
+            case 'CSV (for ISP)':
+                return this._convertToISP(dataArray);
+            default:
+                throw new Error('Unrecognized file format specified');
         }
     },
+
     actions: {
+        /**
+         * Creates a file for the user to download
+         */
         downloadFile() {
-            let blob = new window.Blob([this.get('processedData')], {
+            const blob = new window.Blob([this.get('processedData')], {
                 type: 'text/plain;charset=utf-8'
             });
-            let format = this.get('dataFormat');
-            let extension = this.get('dataFormats')[format].toLowerCase();
+
+            const format = this.get('dataFormat');
+            const extension = this.get('dataFormats')[format].toLowerCase();
+
             window.saveAs(blob, `data.${extension}`);
         },
+
+        /**
+         * Sets the current data format
+         *
+         * @param dataFormat {dataFormats}
+         */
         selectDataFormat(dataFormat) {
             this.set('dataFormat', dataFormat);
         }
--- tests/integration/components/export-tool-test.js
+++ tests/integration/components/export-tool-test.js
@@ -1,10 +1,62 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-
 moduleForComponent('export-tool', 'Integration | Component | export tool', {
     integration: true
 });
+
+/**
+ * Adds double quotes around a string
+ *
+ * @param value {*} - The value to quote
+ * @returns {string}
+ */
+function quote(value) {
+    if (typeof value !== 'string') {
+        return value;
+    }
+
+    return `"${value}"`;
+}
+
+/**
+ * A legible way to define a *SV file in js
+ *
+ * @param arr {!Array<(Array<String>|String)>} - An array of rows
+ * @param columnSeparator - The column separator (defaults to ',')
+ * @returns string
+ */
+function svJoin(arr, columnSeparator) {
+    return arr
+        .map((item, i) =>
+            Array.isArray(item) ?
+                (i > 0 ? item.map(quote) : item).join(columnSeparator) :
+                item
+        )
+        .join('\n');
+}
+
+/**
+ * Runs the export tool component and compares the output
+ *
+ * @param data {Array<Object>} - An array of objects to serialize into the CSV File
+ * @param csv {Array<(Array<String>|String)>|String} - The CSV data
+ * @returns {Function}
+ */
+function runExportTool(data, csv) {
+    return function(assert) {
+        this.set('sanitizeProfileId', sanitizeProfileId);
+        this.set('data', data);
+        this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
+        this.$('.export-tool-select').val('CSV');
+        this.$('.export-tool-select').change();
+
+        assert.strictEqual(
+            this.$('.export-tool-textarea').val(),
+            typeof csv === 'string' ? csv : svJoin(csv)
+        );
+    };
+}
 
 const DATA = [{
     age: 35,
@@ -12,7 +64,10 @@ const DATA = [{
 }];
 
 const sanitizeProfileId = function(session) {
-    session.profileId = session.profileId.split('.').slice(-1)[0];
+    if ('profileId' in session) {
+        session.profileId = session.profileId.split('.').slice(-1)[0];
+    }
+
     return session;
 };
 
@@ -20,74 +75,105 @@ test('JSON format', function(assert) {
     this.set('sanitizeProfileId', sanitizeProfileId);
     this.set('data', DATA);
     this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
-    assert.equal(this.$('.export-tool-textarea').val(), JSON.stringify(DATA, null, 4));
+
+    assert.strictEqual(this.$('.export-tool-textarea').val(), JSON.stringify(DATA, null, 4));
 });
 
-test('CSV format', function(assert) {
-    this.set('sanitizeProfileId', sanitizeProfileId);
-    this.set('data', DATA);
-    this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
-    this.$('.export-tool-select').val('CSV');
-    this.$('.export-tool-select').change();
-    assert.equal(this.$('.export-tool-textarea').val(), 'age,profileId\n35,"test"');
-});
+test('CSV format', runExportTool(
+    DATA,
+    'age,profileId\n35,"test"'
+));
 
-test('CSV format handles commas', function(assert) {
-    this.set('sanitizeProfileId', sanitizeProfileId);
-    const data = [{
+test('CSV format handles commas', runExportTool(
+    [{
         response: 'running, swimming, biking',
         profileId: 'test.test'
-    }];
-    this.set('data', data);
-    this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
-    this.$('.export-tool-select').val('CSV');
-    this.$('.export-tool-select').change();
-    const csv = 'response,profileId\n"running, swimming, biking","test"';
-    assert.equal(this.$('.export-tool-textarea').val(), csv);
-});
+    }],
+    'response,profileId\n"running, swimming, biking","test"'
+));
 
-test('CSV format handles double quotes', function(assert) {
-    this.set('sanitizeProfileId', sanitizeProfileId);
-    const data = [{
+test('CSV format handles double quotes', runExportTool(
+    [{
         response: 'This is a "quote"',
         profileId: 'test.test'
-    }];
-    this.set('data', data);
-    this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
-    this.$('.export-tool-select').val('CSV');
-    this.$('.export-tool-select').change();
-    const csv = 'response,profileId\n"This is a ""quote""","test"';
-    assert.equal(this.$('.export-tool-textarea').val(), csv);
-});
+    }],
+    [
+        ['response', 'profileId'],
+        ['This is a ""quote""', 'test']
+    ]
+));
 
-test('CSV format handles new lines', function(assert) {
-    this.set('sanitizeProfileId', sanitizeProfileId);
-    const data = [{
+test('CSV format handles new lines', runExportTool(
+    [{
         response: 'One line\nAnother line',
         profileId: 'test.test'
-    }];
-    this.set('data', data);
-    this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
-    this.$('.export-tool-select').val('CSV');
-    this.$('.export-tool-select').change();
-    const csv = 'response,profileId\n"One line\\nAnother line","test"';
-    assert.equal(this.$('.export-tool-textarea').val(), csv);
-});
+    }],
+    'response,profileId\n"One line\\nAnother line","test"'
+));
 
-test('CSV format handles non-english characters', function(assert) {
-    this.set('sanitizeProfileId', sanitizeProfileId);
-    const data = [{
+test('CSV format handles non-english characters', runExportTool(
+    [{
         spanish: 'áéíñóúü¿¡',
         french: 'àâæçèëïîôœùûüÿ€',
         russian: 'дфяшйж',
         hindi: 'हिंदी',
         arabic: 'العربية',
         profileId: 'test.test'
-    }];
-    this.set('data', data);
-    this.render(hbs`{{export-tool data=data mappingFunction=sanitizeProfileId}}`);
-    this.$('.export-tool-select').val('CSV');
-    this.$('.export-tool-select').change();
-    const csv = 'spanish,french,russian,hindi,arabic,profileId\n"áéíñóúü¿¡","àâæçèëïîôœùûüÿ€","дфяшйж","हिंदी","العربية","test"';
-    assert.equal(this.$('.export-tool-textarea').val(), csv);
-});
+    }],
+    [
+        ['spanish', 'french', 'russian', 'hindi', 'arabic', 'profileId'],
+        ['áéíñóúü¿¡', 'àâæçèëïîôœùûüÿ€', 'дфяшйж', 'हिंदी', 'العربية', 'test']
+    ]
+));
+
+test('CSV format handles deeply nested object with dot notation', runExportTool(
+    [{
+        alpha: 'one',
+        bravo: 'two',
+        charlie: {
+            delta: 'three'
+        },
+        echo: {
+            foxtrot: {
+                golf: 'four'
+            }
+        },
+        hotel: {
+            india: 'five',
+            juliet: {
+                kilo: 'six'
+            }
+        },
+        lima: {
+            mike: {
+                november: {
+                    oscar: {
+                        papa: {
+                            quebec: 'seven'
+                        }
+                    }
+                }
+            }
+        }
+    }],
+    [
+        [
+            'alpha',
+            'bravo',
+            'charlie.delta',
+            'echo.foxtrot.golf',
+            'hotel.india',
+            'hotel.juliet.kilo',
+            'lima.mike.november.oscar.papa.quebec'
+        ],
+        [
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven'
+        ]
+    ]
+));
